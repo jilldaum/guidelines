398c66fe98392d1a1b13e3de4c5bd41c2d6022cb
jilldaum <support@helpdesktops.org>
2025-06-16 17:22:39 +0530

    Add docs folder

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+# conf.py - required by Read the Docs
+project = 'My Project Name'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
